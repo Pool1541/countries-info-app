9bebcd840b8d6961cc48c8d380fe8ea802e69598
Pool <pool_1541@hotmail.com>
2023-07-28 01:27:42 -0500

    feat(lib) : Se a gregó gh-pages para desplegar el proyecto a producción

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://pool1541.github.io/countries-info-app',
+  build: {
+    outDir: 'build',
+  },
 });
